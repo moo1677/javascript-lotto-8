e3d6fedfb7802784733ef4a25117c5c615c718c5
moo1677 <lgm04@naver.com>
2025-10-31 01:13:54 +0900

    feat: 당첨 번호 입력

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,4 +5,9 @@ export default class InputView {
     const cache = await Console.readLineAsync('구입금액을 입력해 주세요.');
     return cache;
   }
+  async inputWinningLottoNumber() {
+    const winningLottoNumber =
+      await Console.readLineAsync('당첨 번호를 입력해 주세요.');
+    return winningLottoNumber;
+  }
 }
