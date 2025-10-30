26a0c36f17405e3efc3991b3d792008e413ad6be
moo1677 <lgm04@naver.com>
2025-10-31 01:20:30 +0900

    feat: 보너스 번호 입력

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -2,12 +2,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 export default class InputView {
   async inputCache() {
-    const cache = await Console.readLineAsync('구입금액을 입력해 주세요.');
+    const cache = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
     return cache;
   }
   async inputWinningLottoNumber() {
     const winningLottoNumber =
-      await Console.readLineAsync('당첨 번호를 입력해 주세요.');
+      await Console.readLineAsync('당첨 번호를 입력해 주세요.\n');
     return winningLottoNumber;
+  }
+  async inputBonusNumber() {
+    const bonusNumber =
+      await Console.readLineAsync('보너스 번호를 입력해 주세요.\n');
+    return bonusNumber;
   }
 }
