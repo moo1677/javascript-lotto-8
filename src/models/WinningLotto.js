2209c94064a2162a0e6f84fa81da5125c835e6df
moo1677 <lgm04@naver.com>
2025-10-31 02:53:41 +0900

    feat: 보너스 번호 유효성 검사

--- src/models/WinningLotto.js
+++ src/models/WinningLotto.js
@@ -4,6 +4,26 @@ export default class WinningLotto {
   #bonusNumber;
   constructor(winningNumbers, bonusNumber) {
     this.#winningNumbers = new Lotto(winningNumbers);
-    this.#bonusNumber = bonusNumber;
+    this.#bonusNumber = this.#validateBonusNumber(bonusNumber);
+  }
+  #validateBonusNumber(bonusNumber) {
+    this.#validateRange(bonusNumber);
+    this.#validateNoDuplicates(bonusNumber);
+    return bonusNumber;
+  }
+  #validateRange(bonusNumber) {
+    const isValid = (number) =>
+      Number.isInteger(number) &&
+      number >= LOTTO_CONSTANTS.MIN_LOTTO_NUMBER &&
+      number <= LOTTO_CONSTANTS.MAX_LOTTO_NUMBER;
+    if (!isValid(bonusNumber))
+      throw new Error('[ERROR] 보너스 번호는 1에서 45사이의 숫자 입니다.');
+  }
+  #validateNoDuplicates(bonusNumber) {
+    const winnerNumbers = this.#winningNumbers.getNumber();
+    winnerNumbers.forEach((number) => {
+      if (number === bonusNumber) throw new Error('[ERROR]');
+    });
+    throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
   }
 }
