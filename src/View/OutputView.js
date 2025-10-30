610ed5c94c8f579f08591b3eb9debc0f11c7eb94
moo1677 <lgm04@naver.com>
2025-10-31 01:32:06 +0900

    feat: 발행한 로또 수량 출력

---
+++ src/View/OutputView.js
@@ -0,0 +1,7 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export default class OutputView {
+  outputLottoCount(purchaseAmount) {
+    Console.print(`${purchaseAmount / 1000}개를 구매했습니다.`);
+  }
+}
