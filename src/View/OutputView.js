4e21fba2ee9c71e5482dfa11958fb06a9be82ec6
moo1677 <lgm04@naver.com>
2025-10-31 01:39:21 +0900

    feat: 로또 번호 출력

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -4,4 +4,9 @@ export default class OutputView {
   outputLottoCount(purchaseAmount) {
     Console.print(`${purchaseAmount / 1000}개를 구매했습니다.`);
   }
+  outPutLottoNumber(lottoArray) {
+    lottoArray.forEach((lottos) => {
+      Console.print(`[${lottos.number.join(', ')}]`);
+    });
+  }
 }
