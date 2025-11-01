4f1c8fdbd0d8f3bcf899c8fcc55561b8a5fc338d
moo1677 <lgm04@naver.com>
2025-11-01 20:48:54 +0900

    refactor(WinningLotto.js): 검증을 위해 구조 변경

--- src/models/WinningLotto.js
+++ src/models/WinningLotto.js
@@ -1,9 +1,11 @@
 import Lotto from './Lotto';
+import { LOTTO_CONSTANTS } from '../Constant/lottoConstant';
+
 export default class WinningLotto {
-  #winningNumbers;
+  #lotto;
   #bonusNumber;
-  constructor(winningNumbers, bonusNumber) {
-    this.#winningNumbers = new Lotto(winningNumbers);
+  constructor(winningLotto, bonusNumber) {
+    this.#lotto = winningLotto;
     this.#bonusNumber = this.#validateBonusNumber(bonusNumber);
   }
   #validateBonusNumber(bonusNumber) {
@@ -14,16 +16,18 @@ export default class WinningLotto {
   #validateRange(bonusNumber) {
     const isValid = (number) =>
       Number.isInteger(number) &&
-      number >= LOTTO_CONSTANTS.MIN_LOTTO_NUMBER &&
-      number <= LOTTO_CONSTANTS.MAX_LOTTO_NUMBER;
+      number >= LOTTO_CONSTANTS.MIN_NUMBER &&
+      number <= LOTTO_CONSTANTS.MAX_NUMBER;
     if (!isValid(bonusNumber))
       throw new Error('[ERROR] 보너스 번호는 1에서 45사이의 숫자 입니다.');
   }
   #validateNoDuplicates(bonusNumber) {
-    const winnerNumbers = this.#winningNumbers.getNumber();
+    const winnerNumbers = this.#lotto.getNumber();
     winnerNumbers.forEach((number) => {
-      if (number === bonusNumber) throw new Error('[ERROR]');
+      if (number === bonusNumber)
+        throw new Error(
+          '[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.',
+        );
     });
-    throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
   }
 }
