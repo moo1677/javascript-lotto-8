423e3f1fcea63fc606154edd3cc3a2cceffd2c3a
moo1677 <lgm04@naver.com>
2025-11-01 23:03:08 +0900

    feat: 로또 티켓 구매 구현

---
+++ src/models/LottoManager.js
@@ -0,0 +1,33 @@
+import Lotto from './Lotto.js';
+import randomNumbers from '../utils/randomNumbers.js';
+import { LOTTO_CONSTANTS } from '../Constant/lottoConstant.js';
+import OutputView from '../View/OutputView.js';
+
+export default class LottoManager {
+  #lottoCount;
+  #lottos;
+  #winnerLotto;
+  constructor(purchaseAmount, winnerLotto) {
+    this.#lottoCount = purchaseAmount / LOTTO_CONSTANTS.PRICE;
+    this.#lottos = [];
+    this.#winnerLotto = winnerLotto;
+  }
+
+  #createLotto() {
+    for (let i = 0; i < this.#lottoCount; i += 1) {
+      const numbers = randomNumbers();
+      const lotto = new Lotto(numbers);
+      this.#lottos.push(lotto);
+    }
+  }
+  #printLotto() {
+    OutputView.outputLottoCount(this.#lottoCount);
+    this.#lottos.forEach((lotto) => {
+      OutputView.outPutLottoNumber(lotto.getNumber());
+    });
+  }
+  runLottoMachine() {
+    this.#createLotto();
+    this.#printLotto();
+  }
+}
